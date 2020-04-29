1c79de39d35c23bf30eb84e7a25000c333a8591c
lei2rock <zjpjzdl@qq.com>
2020-04-29 15:08:29 +0800

    Uptime Status

---
+++ config.js
@@ -0,0 +1,35 @@
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: '班班实验室 运行状态',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+    'ur904444-a24d1c36ea33d2df250c65f7',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: false,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 60,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://dlzhang.com'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.dlzhang.com'
+    }
+  ]
+};
